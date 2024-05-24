69b99e6e27b99bc265a872c59b61372798a97c3b
Grib145 <93544061+Pisatel2042@users.noreply.github.com>
2024-05-24 15:39:59 +0300

    lab1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+namespace ProgramLabShum
+{
+    class Program{
+
+    static void Main(string[] args)
+    {
+        Console.Write("x=");
+        double x = double.Parse(Console.ReadLine());
+        double answer = 2*Math.Sin(x)+3*Math.Cos(x);
+        Console.WriteLine(answer);
+        Console.ReadLine();
+    }
+
+
+    }
+}
